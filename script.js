7431f45c60c368ca3214722bf16c721e6762f59e
Matheus Arakaki <arakakimath@gmail.com>
2024-09-17 14:16:50 -0400

    setting currency prices and tags

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-//console.log("Javascript carregado!")
+const dollarPrice = 5.50;
+const euroPrice = 6.11;
+const poundPrice = 7.24;
 
 const amount = document.getElementById("amount");
 
@@ -18,4 +20,21 @@ form.onsubmit = (event) => {
   //prevenir o reload
   event.preventDefault();
   //console.log(currency.value);
+
+  switch (currency.value){
+    case "USD":
+      convertCurrency(amount.value, dollarPrice, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, euroPrice, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, poundPrice, "£");
+      break;
+  }
 };
+
+//Função para converter a moeda
+function convertCurrency(amount, price, symbol){
+  console.log(amount, price, symbol);
+}
